fd3957dab8161fac807ea3d4db908be7cd3aa23d
jonas <jonas.schumacher@tu-dortmund.de>
2021-04-16 14:25:36 +0200

    add sun path calculation files

---
+++ CalcTest.java
@@ -0,0 +1,13 @@
+
+public class CalcTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculation cal = new Calculation(52.1,7.6,1,0,1);
+		System.out.println(cal.getSunRise());
+		System.out.println(cal.getSunMax());
+		System.out.println(cal.getSunSet());
+		System.out.println(cal.getAltitudeMax());
+	}
+
+}
